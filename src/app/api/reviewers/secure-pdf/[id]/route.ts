deb8f015ce135930e279faa565795bc663193e4c
gilbert-24task <240129839+gilbert-barprime@users.noreply.github.com>
2026-01-08 10:11:29 +0800

    add vercel.json

--- src/app/api/reviewers/secure-pdf/[id]/route.ts
+++ src/app/api/reviewers/secure-pdf/[id]/route.ts
@@ -33,11 +33,6 @@ export async function GET(
 
     return new NextResponse(buffer, {
       headers: {
-        "Access-Control-Allow-Origin": "http://localhost:3000",
-        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-        "Access-Control-Allow-Headers":
-          "Content-Type, Authorization, X-Custom-Header",
-
         "Content-Type": "application/pdf",
         "Content-Disposition": "inline", // show in iframe
         "Cache-Control": "no-store",
